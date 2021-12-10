c9b04d20b5116f3f0f392099885328ee9fb2e4a6
Ethan Yun <ethanyun@ohs.stanford.edu>
2021-12-09 19:08:28 -0800

    Diagnostic function

---
+++ head.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	// A couple of diagnostic outputs
+	printf("Length of double: %d\r\n", sizeof(double));
+	printf("Length of long: %d\r\n", sizeof(long));
+	printf("Length of int: %d\r\n", sizeof(int));
+
+	return 0;
+}
